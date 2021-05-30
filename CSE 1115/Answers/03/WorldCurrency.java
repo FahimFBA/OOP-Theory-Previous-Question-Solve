09cd410638e1618614a2a445f965522b82c71751
FahimFBA <fahimbinamin@gmail.com>
2021-05-30 12:51:08 +0600

    3(a) unsolved _ gave it to Dip

---
+++ CSE 1115/Answers/03/WorldCurrency.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class WorldCurrency extends JFrame {
+    WorldCurrency(String n){
+        setTitle(n);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400,200);
+
+        JPanel p = new JPanel();
+        JLabel l1 = new JLabel("Euro: ");
+        p.add(l1);
+        JTextField euro = new JTextField("0.0" , 10);
+        p.add(euro);
+        JLabel l2 = new JLabel("USD: ");
+        p.add(l2);
+        JTextField usd = new JTextField("0.0" , 10);
+        p.add(usd);
+        JButton b = new JButton("Get USD value");
+        p.add(b);
+        b.addActionListener(this);
+        setContentPane(p);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==b){
+            usd.setText(Double.parseDouble(usd.getText())*1.16);
+        }
+    }
+
+    public static void main(String[] args){
+        new WorldCurrency ("Currency Converter");
+    }
+}
