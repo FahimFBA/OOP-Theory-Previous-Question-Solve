7b3765cfd1cebe322f2c688762f863bb11bc4ab5
FahimFBA <fahimbinamin@gmail.com>
2021-06-06 21:50:52 +0600

    solved 4(a)

---
+++ CSE 193/Answer/4(a)/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    public void study() {
+        System.out.println("Student is studying");
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.study();
+        Student adnan = new Student() {
+            public void study() {
+                System.out.println("Adnan is studying");
+            }
+        };
+        adnan.study();
+    }
+}
