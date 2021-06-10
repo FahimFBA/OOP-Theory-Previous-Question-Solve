a5b52b701ae8318484674507689fd151ca6ea7df
FahimFBA <fahimbinamin@gmail.com>
2021-06-10 12:47:11 +0600

    solved 2 fully, Alhamdulillah

--- CSE 193/Answer/02/Employee.java
+++ CSE 193/Answer/02/Employee.java
@@ -1,30 +1,76 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 
-public class Employee {
-    public int ID;
-    public String name;
-    public double salary;
+public class E {
+    public static void main(String[] args) {
+        ArrayList<Employee> students1 = new ArrayList<>(); // creating ArrayList
+        try {
+            File f = new File("src/", "employeeDirectory.txt");
+            Scanner sc = new Scanner(f);
 
-    Employee(int a, String b, double c){
+            ArrayList<Employee> employees = new ArrayList<>();
+
+            int i = 0;
+
+            int ID;
+            String name;
+            double salary;
+
+            while (sc.hasNext()) {
+
+                ID = sc.nextInt();
+                name = sc.next();
+                salary = sc.nextDouble();
+
+                Employee s = new Employee(ID, name, salary);
+                employees.add(s);
+
+
+            }
+            employees.sort(new Comparator<Employee>() {
+                @Override
+                public int compare(Employee o1, Employee o2) {
+                    if (o1.salary < o2.salary) return 1;
+                    else if (o1.salary > o2.salary) return -1;
+                    return 0;
+                }
+            });
+
+
+            for (int j = 0; j < 1; j++) {
+                System.out.println(employees.get(j).ID + " " + employees.get(j).name + " " + employees.get(j).salary);
+            }
+
+            sc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Employee {
+    int ID;
+    String name;
+    double salary;
+
+    Employee(int a, String b, double c) {
         this.ID = a;
         this.name = b;
         this.salary = c;
     }
-
-    public static void main(String[] args) {
-        File file = new File("src/" , "employeeDirectory.txt");
-        try {
-            BufferedReader in = new BufferedReader(new FileReader((file)));
-            String str = in.readLine();
-            while (str != null){
-                System.out.println(str);
-                str = in.readLine();
-            }
-            in.close();
-        }catch (FileNotFoundException e){
-            System.out.println("File is not found");
-        }catch (IOException e){
-            System.out.println("Input/Output problem occured");
-        }
-    }
 }
+
+/*
+In employeeDirectory.txt
+
+100 Karim 10030
+200 Rahim 50200
+500 Mina 6500
+1000 Sajib 11000
+201 Rina 50000
+ */
